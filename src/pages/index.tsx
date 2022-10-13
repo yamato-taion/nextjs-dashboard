29cd4385c079c8f654ff46779536e18b20eb8621
tanQ2022 <temperature000045@gmail.com>
2022-10-13 16:15:07 +0900

    index.tsxを変えました

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
       </Head>
       <div className="grid auto-rows-fr grid-cols-1 grid-rows-5 gap-6 md:grid-cols-3 xl:grid-cols-4 xl:grid-rows-4">
         <div className="col-span-1 row-span-3 md:col-span-3 xl:col-span-4">
-          <Card title="入力情報" description="35人が入力中" />
+          <Card title="入力情報" description="20人が入力中" />
         </div>
         <div className="col-span-1 row-span-1 md:col-span-2 md:row-span-3 xl:col-span-2">
           <Card title="体温グラフ" description="グラフ" />
